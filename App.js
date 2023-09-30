06d98f81a6192d8a57460538cdeb233c4e6d440d
Esteban Daniel Rivero <100779672+DaniRC22@users.noreply.github.com>
2023-09-29 22:32:06 -0300

    Pomodoro
    Probando React Native, primera aplicación.
    Contador de 25min para enfocarse dentro de ese rango.
    Contador de 5min para un descanso.
    Contador de 15min para un descanso mas largo.

---
+++ App.js
@@ -0,0 +1,77 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, button } from 'react-native';
+import { useEffect, useState } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+
+const colors = ["#F7DC6F","#A2D9CE","#D7BDE2"]
+
+export default function App() {
+
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("ACTION"| "SHORT" | "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(()=> {
+    let interval = null;
+    if(isActive) {
+      interval = setInterval(()=>{
+        setTime(time - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    if (time===0) {
+      setIsActive(false);
+      setIsWorking((prev) = !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  function handleStartStop(){
+    setIsActive(!isActive);
+  }
+
+  return (
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime]}]}>
+    <View style= {{flex: 1, paddingHorizontal:15, paddingTop: Platform.OS === "android" && 30}} >
+      <Text style={styles.text}>Reloj</Text>
+      <Header currentTime={currentTime}
+      setCurrentTime={setCurrentTime}
+      setTime={setTime}
+      />
+      <Timer time={time}/>
+      <TouchableOpacity 
+      onPress={handleStartStop}
+      style={styles.button}>
+        <Text style={{color: "white", fontWeight:"bold"}}>
+          {isActive ? "STOP" : "START"}
+        </Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  button: {
+    alignItems:"center",
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+  }
+});
